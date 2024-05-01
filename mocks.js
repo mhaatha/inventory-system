bb628405b5cd605bce33356e1f2e4264256bcc6f
mhaatha <mhafidza528@gmail.com>
2024-05-01 20:55:38 +0800

    Restart Project

--- mocks.js
+++
@@ -1,5 +0,0 @@
-// mock.js adalah penyambung antara config jest ke mock database
-global.atob = require('atob');
-
-jest.mock('./prisma/index');
-jest.setTimeout(30000);
--- src/utils/catchAsync.js
+++
@@ -1,5 +0,0 @@
-const catchAsync = (fn) => (req, res, next) => { // parameter fn adalah sebuah fungsi yang akan dibungkus 
-  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-};
-
-module.exports = catchAsync;
